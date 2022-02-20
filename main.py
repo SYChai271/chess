477a7d15787e982377dd8c77d88a07afc0067f74
CSY271 <59222287+SYChai271@users.noreply.github.com>
2022-02-20 14:36:20 +0800

    refactored some code

--- main.py
+++ main.py
@@ -7,27 +7,30 @@ pygame.init()
 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-turn = 'w'
-selected_piece = None
-selected_square = None
-selected_piece_prev = None
-# BOARD constant that does not change
-# BOARD constatnt is in integer form and is used to represent black and white squares on the board
-BOARD = np.zeros((8, 8), dtype=int)
-BOARD[:: 2, 1:: 2] = 1
-BOARD[1:: 2, :: 2] = 1
-# correctly orientate the board
-BOARD = np.flip(BOARD, 0)
-# starting position of all the pieces on the board
-# (0, 0) top left
-# initiate the piece objects that is imported from pieces.py
-b_pieces = {'rook1': Rook((0, 0), 'b'), 'rook2': Rook((7, 0), 'b'), 'knight1': Knight((1, 0), 'b'), 'knight2': Knight((6, 0), 'b'), 'bishop1': Bishop((2, 0), 'b'), 'bishop2': Bishop((5, 0), 'b'), 'queen': Queen((3, 0), 'b'), 'king': King(
-    (4, 0), 'b'), 'pawn1': Pawn((0, 1), 'b'), 'pawn2': Pawn((1, 1), 'b'), 'pawn3': Pawn((2, 1), 'b'), 'pawn4': Pawn((3, 1), 'b'), 'pawn5': Pawn((4, 1), 'b'), 'pawn6': Pawn((5, 1), 'b'), 'pawn7': Pawn((6, 1), 'b'), 'pawn8': Pawn((7, 1), 'b')}
-w_pieces = {'rook1': Rook((0, 7), 'w'), 'rook2': Rook((7, 7), 'w'), 'knight1': Knight((1, 7), 'w'), 'knight2': Knight((6, 7), 'w'), 'bishop1': Bishop((2, 7), 'w'), 'bishop2': Bishop((5, 7), 'w'), 'queen': Queen((3, 7), 'w'), 'king': King(
-    (4, 7), 'w'), 'pawn1': Pawn((0, 6), 'w'), 'pawn2': Pawn((1, 6), 'w'), 'pawn3': Pawn((2, 6), 'w'), 'pawn4': Pawn((3, 6), 'w'), 'pawn5': Pawn((4, 6), 'w'), 'pawn6': Pawn((5, 6), 'w'), 'pawn7': Pawn((6, 6), 'w'), 'pawn8': Pawn((7, 6), 'w')}
 
 
 def on_start():
+    global BOARD, b_pieces, w_pieces, turn, selected_piece, selected_piece_prev, selected_square
+    turn = 'w'
+    selected_piece = None
+    selected_square = None
+    selected_piece_prev = None
+
+    # BOARD constant that does not change
+    # BOARD constatnt is in integer form and is used to represent black and white squares on the board
+    BOARD = np.zeros((8, 8), dtype=int)
+    BOARD[:: 2, 1:: 2] = 1
+    BOARD[1:: 2, :: 2] = 1
+    # correctly orientate the board
+    BOARD = np.flip(BOARD, 0)
+    # starting position of all the pieces on the board
+    # (0, 0) top left
+    # initiate the piece objects that is imported from pieces.py
+    b_pieces = {'rook1': Rook((0, 0), 'b'), 'rook2': Rook((7, 0), 'b'), 'knight1': Knight((1, 0), 'b'), 'knight2': Knight((6, 0), 'b'), 'bishop1': Bishop((2, 0), 'b'), 'bishop2': Bishop((5, 0), 'b'), 'queen': Queen((3, 0), 'b'), 'king': King(
+        (4, 0), 'b'), 'pawn1': Pawn((0, 1), 'b'), 'pawn2': Pawn((1, 1), 'b'), 'pawn3': Pawn((2, 1), 'b'), 'pawn4': Pawn((3, 1), 'b'), 'pawn5': Pawn((4, 1), 'b'), 'pawn6': Pawn((5, 1), 'b'), 'pawn7': Pawn((6, 1), 'b'), 'pawn8': Pawn((7, 1), 'b')}
+    w_pieces = {'rook1': Rook((0, 7), 'w'), 'rook2': Rook((7, 7), 'w'), 'knight1': Knight((1, 7), 'w'), 'knight2': Knight((6, 7), 'w'), 'bishop1': Bishop((2, 7), 'w'), 'bishop2': Bishop((5, 7), 'w'), 'queen': Queen((3, 7), 'w'), 'king': King(
+        (4, 7), 'w'), 'pawn1': Pawn((0, 6), 'w'), 'pawn2': Pawn((1, 6), 'w'), 'pawn3': Pawn((2, 6), 'w'), 'pawn4': Pawn((3, 6), 'w'), 'pawn5': Pawn((4, 6), 'w'), 'pawn6': Pawn((5, 6), 'w'), 'pawn7': Pawn((6, 6), 'w'), 'pawn8': Pawn((7, 6), 'w')}
+
     screen.fill(BG_COLOR)
     draw_board()
     for pieces in b_pieces.values():
@@ -121,7 +124,7 @@ def highlight_selected_piece():
                    selected_piece, selected_piece.pos)
 
 
-def get_click_square(pos):
+def get_clicked_square(pos):
     if pos[0] < 640 and pos[1] < 640:
         return (pos[0] // SQUARE_SIZE, pos[1] // SQUARE_SIZE)
 
@@ -134,7 +137,7 @@ def get_selected_square(pos):
     for pieces in w_pieces.values():
         if pieces.pos == pos and turn == 'w':
             return pieces
-    selected_square = pos
+    return pos
 
 
 def highlight_valid_moves(moves):
@@ -162,21 +165,21 @@ def move(pos, moves):
     global selected_square, selected_piece, selected_piece_prev, turn
     try:
         if pos in moves:
-            if BOARD[selected_piece_prev.pos[0]][selected_piece_prev.pos[1]] == 1:
-                draw_square('w', selected_piece_prev.pos)
-                board[selected_piece_prev.pos[0]
-                      ][selected_piece_prev.pos[1]] = '1'
+            if BOARD[selected_piece.pos[0]][selected_piece.pos[1]] == 1:
+                draw_square('w', selected_piece.pos)
+                board[selected_piece.pos[0]
+                      ][selected_piece.pos[1]] = '1'
             else:
-                draw_square('b', selected_piece_prev.pos)
-                board[selected_piece_prev.pos[0]
-                      ][selected_piece_prev.pos[1]] = '0'
+                draw_square('b', selected_piece.pos)
+                board[selected_piece.pos[0]
+                      ][selected_piece.pos[1]] = '0'
             for move in moves:
                 if BOARD[move[0]][move[1]] == 1:
                     draw_square('w', move)
                 else:
                     draw_square('b', move)
-            draw_piece(selected_piece_prev.color,
-                       selected_piece_prev, pos)
+            draw_piece(selected_piece.color,
+                       selected_piece, pos)
 
             if turn == 'b':
                 turn = 'w'
@@ -200,12 +203,12 @@ def move(pos, moves):
                         del b_pieces[key]
                         break
             try:
-                selected_piece_prev.first = False
+                selected_piece.first = False
             except:
                 pass
-            selected_piece_prev.pos = pos
-            board[selected_piece_prev.pos[0]
-                  ][selected_piece_prev.pos[1]] = selected_piece_prev.color
+            selected_piece.pos = pos
+            board[selected_piece.pos[0]
+                  ][selected_piece.pos[1]] = selected_piece.color
             selected_piece_prev = None
             selected_piece = None
             selected_square = None
@@ -227,16 +230,20 @@ def main():
                 _running = False
                 pygame.quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                position = get_click_square(pygame.mouse.get_pos())
-                selected_piece = get_selected_square(position)
+                position = get_clicked_square(pygame.mouse.get_pos())
+                selected_square = get_selected_square(position)
+                selected_piece = selected_square if selected_square in b_pieces.values(
+                ) or selected_square in w_pieces.values() else selected_piece
                 if selected_piece is not None and selected_piece.color == turn:
                     highlight_selected_piece()
                     moves = selected_piece.valid_moves(board)
                     highlight_valid_moves(moves)
-                if selected_piece_prev is not None:
+                if selected_piece is not None:
                     move(selected_square, moves)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    on_start()
                 selected_piece_prev = selected_piece
-                selected_piece = None
 
         pygame.display.flip()
 
